a6fb3cd733b31beeff8f764940d53445f549305b
Vinicius de Paula <viniciusilvav1@hotmail.com>
2021-01-19 01:26:41 -0300

    paginate

--- src/pages/afterAuthenticate/donors/home/Home.jsx
+++ src/pages/afterAuthenticate/donors/home/Home.jsx
@@ -5,8 +5,6 @@ import styled from 'styled-components'
 /* Components */
 import Header from '../../../default/Header'
 import Footer from '../../../default/Footer'
-import Paginate from '../components/Paginate'
-
 
 /* CSS */
 
@@ -68,12 +66,26 @@ const Frame = styled.div`
     background-position: center;
 `
 
+const ButtonPaginate = styled.button`
+    width: 50px;
+    height: 50px;
+    border-radius: 100px;
+    border: none;
+    background-color: ${({ theme }) => theme.color.salmao};
+    color: #ffffff;
+    font-size: 20px;
+    font-weight: bold;
+    margin: 0 10px 20px 10px;
+
+`
+
 export default class HomeDonor extends Component {
     constructor(props) {
         super(props)
         this.state = {
             ongs: [],
-            pages: {},
+            page: '',
+            last_page: '',
             token: {
                 'Authorization': `Bearer ${localStorage.token}`
             }
@@ -81,32 +93,30 @@ export default class HomeDonor extends Component {
     }
 
     componentDidMount() {
-        // axios.get('http://127.0.0.1:8000/api/info/users/ong/list', { headers })
-        //     .then(response => {
-        //         this.setState({
-        //             ongs: response.data.data,
-        //             pages: {
-        //                 nextPage: response.data.next_page_url
-        //             }
-        //         })
-        //     })
-        //     .catch(error => {
-
-        //     })
-
-        this.request()
-
-    }
-
-    request(page = 1) {
         const headers = this.state.token
-        axios.get(`http://127.0.0.1:8000/api/info/users/ong/list`, {headers} )
+        axios.get(`http://127.0.0.1:8000/api/info/users/ong/list?page`, { headers })
             .then(response => {
+                const page = response.data.current_page
                 this.setState({
                     ongs: response.data.data,
-                    pages: {
-                        nextPage: response.data.next_page_url
-                    }
+                    page: page,
+                    last_page: response.data.last_page
+                })
+            })
+            .catch(error => {
+
+            })
+    }
+
+    request(e, page) {
+        e.preventDefault()
+        const headers = this.state.token
+        axios.get(`http://127.0.0.1:8000/api/info/users/ong/list?page=${page}`, { headers })
+            .then(response => {
+                const page = response.data.current_page
+                this.setState({
+                    ongs: response.data.data,
+                    page: page,
                 })
             })
             .catch(error => {
@@ -117,6 +127,10 @@ export default class HomeDonor extends Component {
 
     cutText(text) {
         return text.split(' ').slice(0, 15).join(' ') + '...'
+    }
+
+    moveScroll() {
+        window.scrollTo(0, 0)
     }
 
     render() {
@@ -148,9 +162,9 @@ export default class HomeDonor extends Component {
                     {this.state.ongs.map((val, ind) => {
                         return (
                             <article key={ind} className="d-flex  p-3">
-                                {console.log(val)}
                                 <Frame style={{ backgroundImage: `url('http://127.0.0.1:8000/storage/${val.img}')` }} className="mr-4" />
                                 <div className="text-right d-flex flex-column justify-content-end align-items-end">
+                                    <p>{val.id}</p>
                                     <h5 className="mb-4">{val.nomeCausaSocial}</h5>
                                     <h1 className="mb-4">{val.nomeFantasia}</h1>
                                     <p className="pl-5 mb-3">{this.cutText(val.descricao)}</p>
@@ -159,9 +173,15 @@ export default class HomeDonor extends Component {
                             </article>
                         )
                     })}
-
-                    <Paginate nextPage={this.state.pages} />
-
+                    {this.state.last_page}
+                    <div>
+                        {this.state.page > 1 &&
+                            <ButtonPaginate onClick={e => this.request(e, parseInt(this.state.page) - 1)}> &lt; </ButtonPaginate>
+                        }
+                        {this.state.page < this.state.last_page &&
+                            <ButtonPaginate onClick={e => this.request(e, parseInt(this.state.page) + 1)}> &gt; </ButtonPaginate>
+                        }
+                    </div>
                 </Wrapper>
                 <Footer />
             </Fragment>
